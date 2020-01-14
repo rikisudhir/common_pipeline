5e1519c2923e2f81828752e99caebc4e56d999f3
rikisudhir <rikisudhir@gmail.com>
2020-01-14 13:45:14 +0530

    Update pipeline_common.groovy

--- pipeline_common.groovy
+++ pipeline_common.groovy
@@ -9,3 +9,48 @@ def addition( def number1, def number2) { // TODO: replace this with common func
 		 
     }
 }
+
+
+def substraction(def number1, def number2){
+
+try{
+    if (number1 < number2){
+    echo "substraction cannot happen"
+ }
+    else {
+    return number1-number2 
+  }
+}   catch(error){
+    echo "ERROR: Failed to substract."
+    throw error
+}
+}
+
+def multiplication(def number1, def number2){
+
+try {
+        return number1*number2
+		
+    } catch (error) {
+         echo "ERROR: Failed to add."
+		 throw error
+		 
+    }
+}
+}
+
+
+def division(def number1, def number2){
+
+try{
+    if (number1=0||number2=0){
+    echo "Division cannot happen"
+ }
+    else {
+    return number1/number2 
+  }
+}   catch(error){
+    echo "ERROR: Failed to substract."
+    throw error
+}
+}
